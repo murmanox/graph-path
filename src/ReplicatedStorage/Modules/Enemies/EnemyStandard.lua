0625a869efffb5e4d046fd610d4a1b8aa8050f80
Murmanox <murmanox@hotmail.com>
2020-08-23 11:23:02 +0100

    can overwrite stats at instantiation

--- src/ReplicatedStorage/Modules/Enemies/EnemyStandard.lua
+++ src/ReplicatedStorage/Modules/Enemies/EnemyStandard.lua
@@ -1,7 +1,7 @@
 local BaseEnemy: BaseEnemy = require(game.ReplicatedStorage.Modules.Enemies.BaseEnemy)
----@type PathingService
-local PathingService = require(game.ServerScriptService.Modules.Services.PathingService)
+local PathingService: PathingService = require(game.ServerScriptService.Modules.Services.PathingService)
 local EnemyStats = require(game.ReplicatedStorage.Configs.EnemyStats)
+local Table = require(game.ReplicatedStorage.Modules.Utility.Table)
 
 ---@class StandardEnemy
 local StandardEnemy = {
@@ -11,9 +11,9 @@ StandardEnemy.__index = StandardEnemy
 setmetatable(StandardEnemy, BaseEnemy)
 
 ---@return StandardEnemy
-function StandardEnemy.new()	
-	---@type StandardEnemy
-	local self = BaseEnemy.new(EnemyStats.Standard)
+function StandardEnemy.new(stats)	
+	local stats = Table.Merge(EnemyStats.Standard, stats)
+	local self: StandardEnemy = BaseEnemy.new(stats)
 	setmetatable(self, StandardEnemy)
 	
 	self.path = PathingService:GetNextPath()
